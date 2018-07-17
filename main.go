a7db8a4083fb534c61805e0c19bd51ec32a312f0
massimo.fornasier <massimo.fornasier@pixartprinting.com>
2018-07-17 12:50:56 +0200

    heroku integration

--- main.go
+++ main.go
@@ -15,30 +15,49 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	bot.HandleFunc("/cowsay {text}", CowHandler)
+	bot.HandleFunc("/griller", GrillerHandler)
+	bot.HandleFunc("/dessert", DessertHandler)
+	bot.HandleFunc("/pay",PayHandler)
+	bot.HandleFunc("/time",TimeHandler)
+	//bot.HandleFunc("/menu", MenuHandler)
+	//bot.HandleFunc("/where",WhereHandler)
 	err = bot.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func CowHandler(m *tbot.Message) {
-	reply := fmt.Sprintf("```\n%s\n```", cowsay(m.Vars["text"]))
-	m.Reply(reply, tbot.WithMarkdown)
+func GrillerHandler(m *tbot.Message){
+	message := `GRIGLIATORI\n (anche alternandosi, a necessità, sapete a chi chiedere il cambio)\n
+	* Silvano\n
+	* Alberto d'este\n
+	* Marco Falcier\n
+	* Julian (Vegetarian grill master)\n
+	* Daniele Scarpa\n
+	* Daniele Crosera\n
+	* Simone Pavlovich\n`
+	m.Reply(message)
 }
 
-func cowsay(text string) string {
-	lineLen := utf8.RuneCountInString(text) + 2
-	topLine := fmt.Sprintf(" %s ", strings.Repeat("_", lineLen))
-	textLine := fmt.Sprintf("< %s >", text)
-	bottomLine := fmt.Sprintf(" %s ", strings.Repeat("-", lineLen))
-	cow := `
-        \   ^__^
-         \  (oo)\_______
-            (__)\       )\/\
-               ||----w |
-               ||     ||
-	`
-	resp := fmt.Sprintf("%s\n%s\n%s%s", topLine, textLine, bottomLine, cow)
-	return resp
+func DessertHandler(m *tbot.Message){
+	message := `CHI PORTERA' QUALCOSA DI DOLCE:\n
+				* Marco Favin\n
+				* Massimo F\n
+				* Simone\n
+				* Julian\n
+				* Alice\n`
+	m.Reply(message)
+}
+
+func TimeHandler(m *tbot.Message){
+	m.Reply("Chi aiuta a preparare dalle 10.00\nchi mangia entro le 12.00")
+}
+
+func PayHandler(m *tbot.Message){
+	message := `
+Menu carne + alcool:\t16.00
+Menu carne NO Alcool:\t14.00
+Menu vegetariano + alcool:\t14:00
+Menu vegetariano NO alcool:\t12:00`
+m.Reply(message)
 }
